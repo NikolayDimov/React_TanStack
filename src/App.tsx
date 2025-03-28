084a48357f2d469bfbfb1bb5a21465b9ea1a8459
Nikolay Dimov <dimovv.nikolay@gmail.com>
2025-03-29 00:58:49 +0200

    feat(reactTask) line hover effect

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import { ReactTask } from "./components/ReactTask";
 import { Route, Routes } from "react-router";
 import { routes } from "./static/routes";
 import HomePage from "./components/HomePage";
+import GlobalStyles from "./utils/GlobalStyles";
 
 
 
 function App() {
   return (
-    <Routes>
-      <Route path={routes.home} element={<HomePage />} />
-      <Route path={routes.htmlTaks} element={<HtmlTask />} />
-      <Route path={routes.cssTask} element={<CssTask />} />
-      <Route path={routes.reactTask} element={<ReactTask />} />
-    </Routes>
+    <>
+      <GlobalStyles />
+      <Routes>
+        <Route path={routes.home} element={<HomePage />} />
+        <Route path={routes.htmlTaks} element={<HtmlTask />} />
+        <Route path={routes.cssTask} element={<CssTask />} />
+        <Route path={routes.reactTask} element={<ReactTask />} />
+      </Routes>
+    </>
   );
 }
 
